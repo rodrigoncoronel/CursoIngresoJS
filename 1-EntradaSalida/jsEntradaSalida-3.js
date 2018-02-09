2e23d6e1145cbd8fac29799fd9db04bc3d94c424
rodrigoncoronel <36281555+rodrigoncoronel@users.noreply.github.com>
2018-02-08 21:53:16 -0300

    ejercicios 3, 4 y 5
    ejercicio 3: document.getElementById, .value 
    ejercicio 4: idem ejercicio 3
    ejercicio 5: concatenacion '+'

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -3,6 +3,12 @@ y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
 	
+	var nombre;
+	//document llama al documento origen
+	//.getElementById especifica que tipo de elemento que va a tomar(en este caso el Id)
+	//*Acordate del CTRL+I el la T para cambiar el TagName a Id
+	//*Acordate del .value
+	//Hay dos formas de tomar datos, por prompt y .getElementById
+	nombre = document.getElementById('elNombre').value; 
+	alert(nombre);
 }
-
-
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,11 @@
 */
 function Mostar()
 {
-	
+	var nombre;
+	nombre = prompt("Ingresa tu nombre");
+	//Hay dos comandos de entrada prompt y document.getElementById 
+	//Hay dos comandos de salida prompt y document.getElementById
+	//Ambos son tanto de entrada como de salida, todo depende de que lado del "=" estan
+	document.getElementById('elNombre').value = nombre;
 }
 
