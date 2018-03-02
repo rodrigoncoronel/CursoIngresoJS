d9c5b8b5a4ad394f8d6f8d6d3d47cf4eec86a907
rodrigoncoronel <36281555+rodrigoncoronel@users.noreply.github.com>
2018-03-02 19:55:32 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,20 @@
 function Mostrar()
 {
+	var numero;
+	var contador = 0;
 
+	numero = prompt("Ingrese un numero: ");
+	numero = parseInt(numero);
+	for (var i = 2; i<numero; i++){
 
+		if (i % 2 == 0) {
+
+			console.log(i);
+			contador++;
+
+		}
+	}
+	console.log("Cantidad de Pares: " + contador);
 
 
 }//FIN DE LA FUNCIÓN
