084692abd823ab7dff9720f1dff3ef4b4e55e192
rodrigoncoronel <36281555+rodrigoncoronel@users.noreply.github.com>
2018-03-07 14:04:19 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,17 +2,52 @@ function Mostrar()
 {
 
 	var contador=0;
-	//declarar contadores y variables 
+	//declarar contadores y variables
+	var numero;
+	var acumuladorNegativos;
+	var acumuladorPositivos;
+	var contadorNegativos = 0;
+	var contadorPositivos = 0;
+	var contadorCeros = 0;
+	var contadorPares = 0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferenciaPositivoNegativo;
+	
 	
 	var respuesta="si";
 
-	while(respuesta!="no")
+	while(respuesta=='si')
 	{
+		numero = prompt("Ingrese un numero");
 		
-	
+		if(numero > 0)
+		{
+			acumuladorPositivos = acumuladorPositivos + numero;
+			contadorPositivos++;
+		}else if (numero < 0)
+		{
+			acumuladorNegativos = acumuladorNegativos + numero;
+			contadorNegativos++;
+		}else
+		{
+			contadorCeros++;
+		}
+		
+		if (numero % 2 == 0)
+		{
+			contadorPares++;
+		}
+		
+		promedioPositivos = acumuladorPositivos/contadorPositivos;
+		promedioNegativos = acumuladorNegativos/contadorNegativos;
+		diferenciaPositivoNegativo = acumuladorPositivos - acumuladorNegativos;
+		
+		
+		respuesta = prompt("Desea seguir ingresando numeros?");
 	}
 
 //Parece jodido.... Enjoy <3
-
+	document.write("Promedio de positivos:");
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,32 @@ function Mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
 	var respuesta='si';
+	var numero;
+	var numeroMaximo = 0;
+	var numeroMinimo = 0;
 
-	while(respuesta!='no')
+	while(respuesta=='si')
 	{
+		numero = prompt("Ingrese un numero");
+		numero = parseInt(numero);
 		
-	
+		if (numero > numeroMaximo || contador==0)
+		{
+			numeroMaximo = numero;
+		}
+		if (numero < numeroMinimo || contador==0)
+		{
+			numeroMinimo = numero;
+		}
+		
+		contador++;
+		
+		respuesta = prompt("Desea seguir ingresando numeros?");
 	}
 
-
+	document.getElementById('maximo').value = numeroMaximo;
+	document.getElementById('minimo').value = numeroMinimo;
 
 
 }//FIN DE LA FUNCIÓN
